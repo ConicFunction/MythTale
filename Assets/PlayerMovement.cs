f8959ab6634dd7a8c9211e6a008a6ca4442c571d
ConicFunction <50002227+ConicFunction@users.noreply.github.com>
2020-01-12 19:49:36 -0500

    Adding files first time
    First commit done from MComputer (as opposed to DComputer and my laptop).

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // Does not depend on the framerate - depends on fixed time (50 times/sec by default)
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
